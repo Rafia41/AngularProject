e63298fbd9b01814324113b0037263c0af2bb477
rNaee <rabianaeem274@gmail.com>
2024-05-18 16:43:49 +0500

    Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwoWayComponent } from './two-way/two-way.component';
+import { StructuresComponent } from './structures/structures.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwoWayComponent,
+    StructuresComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
